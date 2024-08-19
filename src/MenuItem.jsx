7001c551e2df17abf2b425ff76bcfb3842787677
Vasanth Gopal <vasanth1627@gmail.com>
2024-08-19 16:48:16 +0530

    conditional rendering of MenuList component and toggling the children of each label

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import MenuList from "./MenuList";
 
 
-
 export default function MenuItem({ item }) {
-  
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({});
 
+   function handleToggleChildren(getCurrentLabel){
+      setDisplayCurrentChildren({
+        ...displayCurrentChildren,
+        [getCurrentLabel]: !displayCurrentChildren[getCurrentLabel],
+      })
+   }
+        // console.log(getCurrentLabel);
+       console.log(displayCurrentChildren);
   return (
       <li>
-        <p>
-        {item.label}
-        </p>
-        {item && item.children && item.children.length ? 
+        <div style={{display:'flex', gap:'20px'}}>
+          <p>{item.label}</p>
+          {item && item.children && item.children.length  ? <span onClick={()=>handleToggleChildren(item.label)}>+</span>  :null}
+        </div>
+        {item && item.children && item.children.length  && displayCurrentChildren[item.label] ? 
           <MenuList menus={item.children} />
         : null}
       </li>
